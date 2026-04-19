657ccbbf078fe784acc9c74d17aed4a4a2ab2b4a
nguyenxuanson01012007-ship-it <nguyenxuanson01012007@gmail.com>
2026-04-19 23:05:57 +0700

    Update MiniProject.sql

--- MiniProject.sql
+++ MiniProject.sql
@@ -36,10 +36,8 @@ CREATE TABLE ENROLLMENT (
     CourseID VARCHAR(10) NOT NULL,
     EnrollmentDate DATETIME DEFAULT CURRENT_TIMESTAMP,
     
-    -- Khai báo khóa chính kết hợp và khóa ngoại trực tiếp
-    PRIMARY KEY (StudentID, CourseID),
-    FOREIGN KEY (StudentID) REFERENCES STUDENT(StudentID),
-    FOREIGN KEY (CourseID) REFERENCES COURSE(CourseID)
+    foreign key (courseID) references course(courseID),
+    foreign key (studentID) references student(studentID)
 );
 
 -- 5. Bảng Kết quả học tập (Score)
@@ -49,57 +47,70 @@ CREATE TABLE SCORE (
     MidtermScore DECIMAL(4,2) CHECK (MidtermScore >= 0 AND MidtermScore <= 10),
     FinalScore DECIMAL(4,2) CHECK (FinalScore >= 0 AND FinalScore <= 10),
     
-    -- Khai báo khóa chính kết hợp và khóa ngoại trực tiếp
-    PRIMARY KEY (StudentID, CourseID),
-    FOREIGN KEY (StudentID, CourseID) REFERENCES ENROLLMENT(StudentID, CourseID)
+    foreign key (studentID) references student(studentID),
+    foreign key (courseID) references course(courseID)
 );
-
 -- Phần II: Nhập dữ liệu ban đầu
 
-INSERT INTO STUDENT (FullName, DateOfBirth, Email) VALUES
-('Nguyen Van A', '2001-05-12', 'a.nguyen@email.com'),
-('Tran Thi B', '2002-08-22', 'b.tran@email.com'),
-('Le Van C', '2000-11-05', 'c.le@email.com'),
-('Pham Thi D', '2003-01-15', 'd.pham@email.com'),
-('Hoang Van E', '2001-09-30', 'e.hoang@email.com');
+-- STUDENT
+INSERT INTO STUDENT (StudentID, FullName, DateOfBirth, Email) VALUES
+('SV01','Nguyen Van A', '2001-05-12', 'a.nguyen@email.com'),
+('SV02','Tran Thi B', '2002-08-22', 'b.tran@email.com'),
+('SV03','Le Van C', '2000-11-05', 'c.le@email.com'),
+('SV04','Pham Thi D', '2003-01-15', 'd.pham@email.com'),
+('SV05','Hoang Van E', '2001-09-30', 'e.hoang@email.com');
 
-INSERT INTO TEACHER (FullName, Email) VALUES
-('TS. Nguyen Tien F', 'f.nguyen@uni.edu'),
-('ThS. Tran Dao G', 'g.tran@uni.edu'),
-('GS. Le Ba H', 'h.le@uni.edu'),
-('TS. Pham Tuan I', 'i.pham@uni.edu'),
-('ThS. Vuong Ngoc K', 'k.vuong@uni.edu');
+-- TEACHER
+INSERT INTO TEACHER (TeacherID, FullName, Email) VALUES
+('GC01','TS. Nguyen Tien F', 'f.nguyen@uni.edu'),
+('GC02','ThS. Tran Dao G', 'g.tran@uni.edu'),
+('GC03','GS. Le Ba H', 'h.le@uni.edu'),
+('GC04','TS. Pham Tuan I', 'i.pham@uni.edu'),
+('GC05','ThS. Vuong Ngoc K', 'k.vuong@uni.edu');
 
-INSERT INTO COURSE (CourseName, Description, TotalSessions, TeacherID) VALUES
-('Toan Cao Cap 1', 'Dai so tuyen tinh', 15, 1),
-('Lap Trinh C++', 'Co ban den nang cao', 30, 2),
-('Co So Du Lieu', 'SQL va Relational DB', 20, 3),
-('Ky Nang Mem', 'Thuyet trinh va Giao tiep', 10, 4),
-('Cau Truc Du Lieu', 'CTDL va Thuat Toan', 25, 2);
+-- COURSE
+INSERT INTO COURSE (CourseID, CourseName, Description, TotalSessions, TeacherID) VALUES
+('K01','Toan Cao Cap 1', 'Dai so tuyen tinh', 15, 'GC01'),
+('K02','Lap Trinh C++', 'Co ban den nang cao', 30, 'GC02'),
+('K03','Co So Du Lieu', 'SQL va Relational DB', 20, 'GC03'),
+('K04','Ky Nang Mem', 'Thuyet trinh va Giao tiep', 10, 'GC04'),
+('K05','Cau Truc Du Lieu', 'CTDL va Thuat Toan', 25, 'GC02');
 
+-- ENROLLMENT
 INSERT INTO ENROLLMENT (StudentID, CourseID, EnrollmentDate) VALUES
-(1, 2, '2026-01-10'), 
-(1, 3, '2026-01-11'), 
-(2, 3, '2026-01-12'), 
-(3, 1, '2026-01-15'), 
-(4, 4, '2026-01-16'); 
+('SV01', 'K02', '2026-01-10'),
+('SV01', 'K03', '2026-01-11'),
+('SV02', 'K03', '2026-01-12'),
+('SV03', 'K01', '2026-01-15'),
+('SV04', 'K04', '2026-01-16');
 
+-- SCORE
 INSERT INTO SCORE (StudentID, CourseID, MidtermScore, FinalScore) VALUES
-(1, 2, 8.5, 9.0),  
-(1, 3, 7.0, 8.5),  
-(2, 3, 6.5, 7.0),  
-(3, 1, 9.0, 9.5);
-
+('SV01', 'K02', 8.5, 9.0),
+('SV01', 'K03', 7.0, 8.5),
+('SV02', 'K03', 6.5, 7.0),
+('SV03', 'K01', 9.0, 9.5);
 -- PHẦN III: CẬP NHẬT DỮ LIỆU
 
-UPDATE STUDENT SET Email = 'a.nguyen_updated@email.com' WHERE StudentID = 1;
-UPDATE COURSE SET Description = 'Khoa hoc C++ version 2026' WHERE CourseID = 2;
-UPDATE SCORE SET FinalScore = 9.5 WHERE StudentID = 1 AND CourseID = 2;
+UPDATE STUDENT 
+SET Email = 'a.nguyen_updated@email.com' 
+WHERE StudentID = 'SV01';
+
+UPDATE COURSE 
+SET Description = 'Khoa hoc C++ version 2026' 
+WHERE CourseID = 'K02';
+
+UPDATE SCORE 
+SET FinalScore = 9.5 
+WHERE StudentID = 'SV01' AND CourseID = 'K02';
 
 -- PHẦN IV: XÓA DỮ LIỆU
 
-DELETE FROM SCORE WHERE StudentID = 3 AND CourseID = 1;
-DELETE FROM ENROLLMENT WHERE StudentID = 3 AND CourseID = 1;
+DELETE FROM SCORE 
+WHERE StudentID = 'SV03' AND CourseID = 'K01';
+
+DELETE FROM ENROLLMENT 
+WHERE StudentID = 'SV03' AND CourseID = 'K01';
 
 -- PHẦN V: TRUY VẤN DỮ LIỆU
 
